89f67d7e10c79f86cac1dc98f013b16222009a60
Tarun Lalwani <tarunlalwani2002@gmail.com>
2023-11-26 11:51:21 +0530

    db creation route

--- Controllers/task.js
+++ Controllers/task.js
@@ -4,6 +4,6 @@ exports.createDB = (req, res) => {
     let q = 'CREATE DATABASE taskmanagement';
     db.query(q, (err, result) => {
         if(err) throw err;
-        return res.status(201).json("DB created")
+        return res.status(201).json("DB taskmanagement created")
     } )
 }
--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host:"localhost",
     user: "tarun",
     password: "123456789",
-    database: "test"
+    database: "taskmanagement"
 
 });
 
