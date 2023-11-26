9b560e3680f6a8b8f7daac47e8836f9d98b94dae
Tarun Lalwani <tarunlalwani2002@gmail.com>
2023-11-26 13:03:22 +0530

    all routes added

--- Controllers/task.js
+++ Controllers/task.js
@@ -7,3 +7,53 @@ exports.createDB = (req, res) => {
         return res.status(201).json("DB taskmanagement created")
     } )
 }
+
+exports.createTable = (req,res) => {
+    let q = 'CREATE TABLE tasks(`id` int AUTO_INCREMENT, `taskname` VARCHAR(45), `desc` VARCHAR(200), PRIMARY KEY(id))';
+    db.query(q, (err, result) => {
+        if(err) throw err;
+        return res.status(201).json("TABLE tasks created")
+    })
+}
+
+exports.createList = (req, res) => {
+    const q = "INSERT INTO tasks SET ?";
+    
+    const { taskname, desc} = req.body;
+
+    db.query(q, {taskname, desc}, (err, result) => {
+        if (err) return res.json(err);
+        return res.status(200).json(result);
+    })
+}
+
+exports.showTasks = (req, res) => {
+    const q = "SELECT * FROM tasks";
+
+    db.query(q, (err, result) => {
+        if(err) return res.json(err);
+        return res.status(200).json(result);
+    })
+}
+
+exports.singleTask = (req,res) => {
+    const q = `SELECT * FROM tasks WHERE id=${req.params.id}`;
+    db.query(q, (err, result) => {
+        
+
+        db.query(q, (err, result) => {
+            if(err) return res.json(err);
+            return res.status(200).json(result[0]);
+        })
+    })
+}
+
+exports.updateTask = (req, res) => {
+    const {taskname, desc} = req.body;
+    const q = `UPDATE tasks SET ? WHERE id=${req.params.id}`;
+
+    db.query(q, {taskname, desc}, (err, result) => {
+        if(err) return res.json(err);
+        return res.status(200).json(result);
+    })
+}
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,8 +1,13 @@
 const express = require('express'); 
 const router = express.Router(); 
-const { createDB } = require('../Controllers/task');
+const { createDB, createTable, createList, showTasks, singleTask, updateTask } = require('../Controllers/task');
 
 
 router.get('/create/database', createDB)
+router.get('/create/table', createTable)
+router.post('/create/list', createList) 
+router.get('/show/tasks', showTasks) 
+router.get('/task/:id', singleTask) 
+router.put('/update/task/:id', updateTask) 
 
 module.exports = router
